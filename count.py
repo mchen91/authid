86b79ea43112bc9dea16da43ffdc3eddfc851b29
Michael Chen <mchen@datadoghq.com>
2014-04-03 21:41:19 -0400

    count specific punctuation

--- count.py
+++ count.py
@@ -5,7 +5,7 @@ import string
 punctuationCount = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 
 # how long is a long word?
-longword = 10
+longword = 11
 
 # util file to figure out which ngrams to use as inputs
 common_ngrams = set()
@@ -44,22 +44,22 @@ for iden, (auth, num_poems) in enumerate(author_poems.items(), start=1):
             length = min(longword, len(word.strip(string.punctuation)))
             word_len_freqs[length] += 1
 
-        puncCount = punctuationCount(text,string.punctuation)/float(text.count('.') + text.count('!') + text.count('?'))
-        commaPerSentence = float(text.count(',')) / (text.count('.') + text.count('!') + text.count('?'))
+        #puncCount = punctuationCount(text,string.punctuation) / float(text.count('.') + text.count('!') + text.count('?'))
+        #commaPerSentence = thingsPerSentence(',', text)
+        punc_freqs = (float(text.count(p)) / len(text) for p in '.,-;\'"')
         text = text.translate(string.maketrans('', ''), string.punctuation)
         trigrams = [''.join(i) for i in ngrams(text, 3)]
 
         for t in trigrams:
             all_ngrams[t] = all_ngrams.get(t, 0) + 1
 
-        total = sum(all_ngrams.values())
-        ngram_freqs = (all_ngrams.get(i, 0) / float(total) for i in common_ngrams)
+        ngram_freqs = (all_ngrams.get(i, 0) / float(sum(all_ngrams.values())) for i in common_ngrams)
         len_freqs = (word_len_freqs[i] / float(sum(word_len_freqs)) for i in range(1, longword + 1))
         csv.write('{},'.format(iden))
         csv.write(','.join(str(f) for f in ngram_freqs))
         csv.write(',')
         csv.write(','.join(str(f) for f in len_freqs))
         csv.write(',')
-        csv.write('{},{}'.format(puncCount, commaPerSentence))
+        csv.write(','.join(str(f) for f in punc_freqs))
         csv.write('\n')
 csv.close()
